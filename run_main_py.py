eefba0d271fc7b14d8d8ce2255ecf06cd82139af
xin.zhang@melot.cn <xin.zhang@melot.cn>
2020-05-21 15:58:53 +0800

    python

---
+++ run_main_py.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+import pytest
+import pytest_html
+import os
+
+if __name__ == '__main__':
+    os.system('pytest --html=./report/report.html')
+    os.system('pytest --reruns1 test_user.py --html =../ reports / test_user.html')
